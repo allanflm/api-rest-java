010fad6a9fa3dfe0d3dea76cbab44ddd54704e77
Allan <allanfelipedk@gmail.com>
2024-04-22 20:30:34 -0300

    feat: modeling the screen match

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Esse é o Screen Match!");
-        System.out.println("Filme: Blade Runner 2049");
+        Movie myMovie = new Movie();
+        myMovie.name = "Blade runner 2049";
+        myMovie.releaseYear = 2017;
 
-        int anoDeLancamento = 2017;
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        myMovie.filmTechnicalSheet();
+        myMovie.filmReview(10);
+        myMovie.filmReview(8);
+        myMovie.filmReview(7);
+        System.out.println("return the evaluation: " + myMovie.returnTheEvaluation());
 
-        boolean incluidoNoPlano = true;
-        System.out.println("Esse filme esta incluido no plano: " + incluidoNoPlano);
-
-        double notaDoFilme = 8.0;
-        System.out.println("Nota do filme: " + notaDoFilme);
-
-        // Calculando a media do filme:
-        double media = (9.4 + 8.0 + 6.4) / 3;
-        System.out.println("Média do filme: " + media);
-
-        String sinopse;
-        sinopse = """
-                Filme: Blade Runner 2049
-                Sinopse: é um filme de ficção cientifica.
-                Ano de lançamento:
-                """ + anoDeLancamento;
-        System.out.println(sinopse);
-
-        int classificacao = (int) (media /  2);
-        String classificacaoString = String.format("Esse filme tem %d estrelas", classificacao);
-        System.out.println(classificacaoString);
     }
 }
