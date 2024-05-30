4898be373e744da77f822b1f2b96a8e760b34e2e
Allan <allanfelipedk@gmail.com>
2024-05-30 19:56:11 -0300

    feat:displaying the top 5 series and creating a method to convert from Series to SeriesDTO

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,16 +1,16 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
@@ -19,5 +19,9 @@ public class SerieController {
     @GetMapping("/series")
     public List<SerieDTO> obterSeries() {
         return service.obterTodasAsSeries();
+    }
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series(){
+        return service.obterTop5Series();
     }
 }
